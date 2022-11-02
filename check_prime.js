b371269db5d63143af7e01f88befeeda0671f0f4
vickysarkar7 <vickyysarkar@gmail.com>
2022-11-02 12:25:29 +0530

    check prime file created and code added as well

---
+++ check_prime.js
@@ -0,0 +1,24 @@
+
+//code writen by Vicky Sarkar
+
+function checkPrime(number)
+let factor=0;
+for(let i=0;i<=number;i++){
+	if(number%i==0){
+		facaator++
+	}
+
+	if(factor==2){
+		return true
+	}else{
+		return false
+	}
+}
+
+let ans=checkPrime(30)
+
+if(ans==true){
+console.log(ans," Is a Prime Number")
+}else{
+	console.log(ans,"Is not a prime Number")
+}
